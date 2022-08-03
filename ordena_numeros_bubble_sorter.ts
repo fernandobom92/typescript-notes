1c4c4648dbdd066eda51cf2ace0b0953bbafa1d4
fernandobom92 <106743747+fernandobom92@users.noreply.github.com>
2022-08-03 12:43:58 -0300

    Update ordena_numeros_bubble_sorter.ts

--- ordena_numeros_bubble_sorter.ts
+++ ordena_numeros_bubble_sorter.ts
@@ -2,12 +2,12 @@ class Sorter {
   constructor(public collection: number[]) {}
 
   sort(): void {
-    const { length } = this.collection; //cria uma variavel e pega o valor de length vindo de collection
+    const { length } = this.collection;
 
-    for (let i = 0; i < length; i++) { //percorre todo o array
-      for (let j = 0; j < length - i - 1; j++) { //percorre excluindo o ultimo da direita
-        if (this.collection[j] > this.collection[j + 1]) { //compara os 2 ultimos numeros da direita
-          const leftNum = this.collection[j]; //armazena o menor numero entre os 2
+    for(let i=0; i<length; i++) { //faz o loop até o fim do array
+      for(let j=0; j < length - i - 1; j++) { //exclui o ultimo da direita a cada loop
+        if(this.collection[j] > this.collection[j+1]) { //compara para achar o maior numero
+          const leftNum = this.collection[j]; //armazena o menor numero entre os 2 numeros
           this.collection[j] = this.collection[j+1]; //joga o valor do maior numero para o fim do array
           this.collection[j+1] = leftNum; //joga o valor do menor numero para a esquerda
         }
@@ -16,6 +16,6 @@ class Sorter {
   }
 }
 
-const sorter = new Sorter([10, -7, 8, 14]);
-sorter.sort(); //passa o array pela função sort
+const sorter = new Sorter([90, -35, 56, -7, 67, 100, -56]);
+sorter.sort();
 console.log(sorter.collection);
